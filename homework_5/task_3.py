ca3c7290cae4e904579631cada1080333500e1a2
VadimVasinAndreevich <132100792+VadimVasinAndreevich@users.noreply.github.com>
2023-08-10 15:43:33 +0500

    Add files via upload

---
+++ homework_5/task_3.py
@@ -0,0 +1,15 @@
+"""
+Создайте функцию генератор чисел Фибоначчи
+"""
+
+
+def fib(x=0, y=1):
+    for el in range(0, 100):
+        el = x + y
+        x = y
+        y = el
+        yield el
+
+
+res = fib()
+print([el for el in res])
